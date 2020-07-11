171d292b6380e0ff063bc51849d29d35a6293a5f
ramiandrasoaf <52719701+ramiandrasoaf@users.noreply.github.com>
2020-07-11 17:31:51 +0300

    Add files via upload

---
+++ ProgPuissance4.java
@@ -0,0 +1,11 @@
+package projet;
+import javax.swing.*;
+
+public class ProgPuissance4 {
+
+	public static void main(String[] args) {
+		JFrame fenetre=new Puissance4();
+		fenetre.setVisible(true);
+	}
+
+}
