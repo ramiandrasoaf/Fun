a457b421b0ccba747b356ff853365cf9811b5c02
nirolivier <nirolivier@gmail.com>
2020-07-11 18:19:46 +0200

    Refactoring

--- src/projet/Puissance4.java
+++ src/projet/Puissance4.java
@@ -1,64 +1,69 @@
 package projet;
-import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.text.AttributedCharacterIterator;
-import java.awt.event.MouseAdapter;
-import javax.swing.*;
 
-public class Puissance4 extends JFrame{
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
+
+public class Puissance4 extends JFrame {
 	private static final long serialVersionUID = 3660342240433840428L;
-	private final static int vide=0;
-	private final static int rouge=1;
-	private final static int jaune=2;
-	private static int couleur,tour;
-	private static JLabel tourJoueur;
-	private static Graphics graph;
 	
+	private static int tour;
+	private static final int COLUMN = 7;
+	private static final int ROW = 6;
+
 	public Puissance4() {
-	    super("Puissance 4");
-	    setSize(590,600);
-	    this.setLocationRelativeTo(null);
-	    this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-	    JMenuBar barreMenu=new JMenuBar();
-	    setJMenuBar(barreMenu);
-	    JMenu modeJeu=new JMenu("Mode de jeu");
-	    JRadioButtonMenuItem homVsMach =new JRadioButtonMenuItem("Homme vs Machine");
-	    JRadioButtonMenuItem machVsHom=new JRadioButtonMenuItem("Machine vs Homme");
-	    JRadioButtonMenuItem homVsHom=new JRadioButtonMenuItem("Homme vs Homme");
-	    JRadioButtonMenuItem machVsMach=new JRadioButtonMenuItem("Machine vs Machine");
-	    ButtonGroup gr=new ButtonGroup();
-	    gr.add(homVsHom);
-	    gr.add(homVsMach);
-	    gr.add(machVsHom);
-	    gr.add(machVsMach);
-	    modeJeu.add(homVsHom);
-	    modeJeu.add( homVsMach);
-	    modeJeu.add( machVsHom);
-	    modeJeu.add(machVsMach);
-	    barreMenu.add(modeJeu);
-	    
-	    tourJoueur=new JLabel("Joueur "+", à vous de jouer !");
-	    barreMenu.add(tourJoueur);
-	    JPanel jeu=new JPanel();
-	    jeu.setSize(400, 300);
-	    jeu.setLayout(new GridLayout(6,7));
-	    jeu.setBackground(Color.gray);
-	    Panneau grille [][]=new Panneau[6][7];
-	    
-	    Jouer jouer = new Jouer();
-	    for(int i=0;i<grille.length;i++) {
-	    	for(int j=0;j<grille.length;j++) {
-	    		grille[i][j]=new Panneau(jouer);
-	    	jeu.add(grille[i][j]);
-	    	}
-	    }
-	    
-	    this.getContentPane().add(jeu,BorderLayout.CENTER);
-	   
+		super("Puissance 4");
+		setSize(590, 600);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		JMenuBar barreMenu = new JMenuBar();
+		setJMenuBar(barreMenu);
+		JMenu modeJeu = new JMenu("Mode de jeu");
+		JRadioButtonMenuItem homVsMach = new JRadioButtonMenuItem("Homme vs Machine");
+		JRadioButtonMenuItem machVsHom = new JRadioButtonMenuItem("Machine vs Homme");
+		JRadioButtonMenuItem homVsHom = new JRadioButtonMenuItem("Homme vs Homme");
+		JRadioButtonMenuItem machVsMach = new JRadioButtonMenuItem("Machine vs Machine");
+		ButtonGroup gr = new ButtonGroup();
+		gr.add(homVsHom);
+		gr.add(homVsMach);
+		gr.add(machVsHom);
+		gr.add(machVsMach);
+		modeJeu.add(homVsHom);
+		modeJeu.add(homVsMach);
+		modeJeu.add(machVsHom);
+		modeJeu.add(machVsMach);
+		barreMenu.add(modeJeu);
+
+		JLabel tourJoueur = new JLabel("Joueur " + ", à vous de jouer !");
+		barreMenu.add(tourJoueur);
+		JPanel jeu = new JPanel();
+		jeu.setSize(400, 300);
+		jeu.setLayout(new GridLayout(6, 7));
+		jeu.setBackground(Color.GRAY);
+
+		Jouer jouer = new Jouer(tourJoueur);
+		
+		for (int i = 0; i < ROW; i++) {
+			for (int j = 0; j < COLUMN; j++) {
+				jeu.add(new Panneau(jouer));
+			}
+		}
+
+		this.getContentPane().add(jeu, BorderLayout.CENTER);
+
 	}
-	
+
 	public class Panneau extends JPanel {
 		private static final long serialVersionUID = 1L;
 
@@ -66,19 +71,13 @@ public class Puissance4 extends JFrame{
 			this.addMouseListener(adapter);
 		}
 
-		public void colorer(Graphics g,int color) {	
-			if(color==rouge) {
-				g.setColor(Color.RED);
-			}else if (color==jaune) {
-				g.setColor(Color.YELLOW);
-			}else {
-				g.setColor(Color.WHITE);
-			}
-			super.paintComponent(g);
-			super.setBackground(Color.BLUE);
+		public void colorer(Graphics g, Color color) {	
+			paintComponent(g);	
+			g.setColor(color);
 			g.fillOval(15, 10, 60, 60);
-			
 		}
+
+		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			super.setBackground(Color.BLUE);
@@ -86,28 +85,27 @@ public class Puissance4 extends JFrame{
 			g.fillOval(15, 10, 60, 60);
 		};
 	}
-	
-	class Jouer extends MouseAdapter{
+
+	class Jouer extends MouseAdapter {
+		
+		private JLabel tourJoueur;
+		
+		public Jouer(JLabel tourJoueur) {
+			this.tourJoueur = tourJoueur;
+		}
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			Graphics g;
 			tour++;
-			String tr=Integer.toString(tour);
-			tourJoueur.setText(tr);
-			Panneau pan= (Panneau) e.getSource();
-			g=pan.getGraphics();
-			
-			if(tour%2==1) {
-				couleur=rouge;
-				pan.colorer(g,rouge);
-			}else if (tour%2==0) {
-				couleur=jaune;
-				pan.colorer(g,jaune);
-			}
+			tourJoueur.setText(Integer.toString(tour));
+			Panneau pan = (Panneau) e.getSource();
+			g = pan.getGraphics();
+			pan.colorer(g, Color.YELLOW);
 
-					
-					
+			if (tour % 2 == 1) {
+				pan.colorer(g, Color.RED);
+			} 
 		}
 	}
 }
